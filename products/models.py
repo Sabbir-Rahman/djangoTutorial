cc1d18b59fbfb844ce0460671d62f057722a14c4
Md Sabbir Rahman <56318666+Sabbir-Rahman@users.noreply.github.com>
2021-01-05 22:57:55 +0600

    add column in the model without delete previous data

--- products/models.py
+++ products/models.py
@@ -6,3 +6,4 @@ class Product(models.Model):
     description = models.TextField(blank=True, null=True) #can be null or blank
     price = models.DecimalField(decimal_places=2, max_digits=10000)
     summary = models.TextField(default='This is summary')
+    featured = models.BooleanField(default=True) #null = True , default = True
