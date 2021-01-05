96301d862adf131a7ae546503b8c997c244eb140
Md Sabbir Rahman <56318666+Sabbir-Rahman@users.noreply.github.com>
2021-01-05 21:35:20 +0600

    a model named product is added and operationable from database

--- django_tutorial/settings.py
+++ django_tutorial/settings.py
@@ -40,6 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # third party
+
+    #own
+    'products',
 ]
 
 MIDDLEWARE = [
