cc75394b6950405db53595db899cd4414c9622d0
Md Sabbir Rahman <56318666+Sabbir-Rahman@users.noreply.github.com>
2021-01-06 02:09:53 +0600

    condition

--- pages/views.py
+++ pages/views.py
@@ -14,7 +14,8 @@ def about_view(request,*args, **kwargs):
     my_context = {
         "my_text": "This is about us",
         "my_number": 123,
-        "my_list": [123, 4242, 12313,"ABCD"]
+        "my_list": [123, 4242, 12313,"ABCD"],
+        "check_list": [2,1,3,5,6,8,4,9,11],
 
     }
     return render(request, "about.html", my_context)
