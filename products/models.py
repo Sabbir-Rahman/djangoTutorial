c3521058d8190f74e17520bd90df2a95af11b345
Md Sabbir Rahman <56318666+Sabbir-Rahman@users.noreply.github.com>
2021-01-05 22:47:29 +0600

    Redefined model with data types

--- products/models.py
+++ products/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    title = models.TextField()
-    description = models.TextField()
-    price = models.TextField()
-    summary = models.TextField(default='This is summary');
+    title = models.CharField(max_length=120) # max field must be declared
+    description = models.TextField(blank=True, null=True) #can be null or blank
+    price = models.DecimalField(decimal_places=2, max_digits=10000)
+    summary = models.TextField(default='This is summary')
