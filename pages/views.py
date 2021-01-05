a6d0a925cb35088ca494208d58ec3987297b01f5
Md Sabbir Rahman <56318666+Sabbir-Rahman@users.noreply.github.com>
2021-01-06 00:35:28 +0600

    url view with templates

--- pages/views.py
+++ pages/views.py
@@ -4,7 +4,11 @@ from django.http import HttpResponse
 
 def home_view(request,*args, **kwargs):
     print(request.user)
-    return HttpResponse("<h1>Hello World</h1>") #string of html code
+    #return HttpResponse("<h1>Hello World</h1>") #string of html code
+    return render(request, "home.html", {})
 
-def contact_view(*args, **kwargs):
-    return HttpResponse("<h1>Contact page</h1>") #string of html code
+def contact_view(request,*args, **kwargs):
+    return render(request, "contact.html", {})
+
+def about_view(request,*args, **kwargs):
+    return render(request, "home.html", {})
