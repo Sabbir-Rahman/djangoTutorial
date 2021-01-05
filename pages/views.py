e33dc72595cc9b3771bb4592f7d17a4899178d84
Md Sabbir Rahman <56318666+Sabbir-Rahman@users.noreply.github.com>
2021-01-06 01:55:56 +0600

    for loop in templates

--- pages/views.py
+++ pages/views.py
@@ -14,7 +14,7 @@ def about_view(request,*args, **kwargs):
     my_context = {
         "my_text": "This is about us",
         "my_number": 123,
-        "my_list": [123, 4242, 12313]
+        "my_list": [123, 4242, 12313,"ABCD"]
 
     }
     return render(request, "about.html", my_context)
