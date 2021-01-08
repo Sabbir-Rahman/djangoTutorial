123636cf438c0c4e87788900434edacadc3ec58c
Md Sabbir Rahman <56318666+Sabbir-Rahman@users.noreply.github.com>
2021-01-08 23:55:57 +0600

    form add with validation and first time no warning show

--- products/views.py
+++ products/views.py
@@ -8,6 +8,8 @@ from .form import ProductForm, RawProductForm
 
 def product_create_view(request):
     my_form = RawProductForm()
+    if request.method == "POST":
+        my_form = RawProductForm(request.POST)
     context = {
         "form": my_form
     }
