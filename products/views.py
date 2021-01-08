0dfc3afbe767ccb9d03f8a2db0fe591fc87d5d7e
Md Sabbir Rahman <56318666+Sabbir-Rahman@users.noreply.github.com>
2021-01-09 00:08:42 +0600

    product save into database with pure form

--- products/views.py
+++ products/views.py
@@ -13,6 +13,7 @@ def product_create_view(request):
         if my_form.is_valid():
             #now it is good data
             print(my_form.cleaned_data)
+            Product.objects.create(**my_form.cleaned_data)
         else:
             print(my_form.errors)
     context = {
