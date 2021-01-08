134f23cedbe57a74ad9d3d54650e06c8379e2bc7
Md Sabbir Rahman <56318666+Sabbir-Rahman@users.noreply.github.com>
2021-01-08 23:40:43 +0600

    save data from raw html form

--- products/views.py
+++ products/views.py
@@ -8,6 +8,13 @@ from .form import ProductForm
 
 #for raw form
 def product_create_view(request):
+    #print(request.GET)
+    #print(request.POST)
+
+    if request.method == "POST":
+        my_new_title = request.POST.get('title')
+        #Product.objects.create(title=my_new_title) here uncomment will save dat abut first data is null
+        print(my_new_title)
     context = {}
     return render(request,"product_create.html", context);
 
