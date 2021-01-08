13835a46d3cadaa4d8ae010229c6432f279bee36
Md Sabbir Rahman <56318666+Sabbir-Rahman@users.noreply.github.com>
2021-01-08 21:35:52 +0600

    Render product details from data base with the help of id

--- products/views.py
+++ products/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
+def product_detail_view(request):
+    obj = Product.objects.get(id=2)
+    context = {
+        'title':obj.title,
+        'description': obj.description
+    }
+    return render(request, "detail.html", context)
