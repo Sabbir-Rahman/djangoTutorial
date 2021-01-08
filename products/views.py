2e38144dd73d805024134273f4595a292faee471
Md Sabbir Rahman <56318666+Sabbir-Rahman@users.noreply.github.com>
2021-01-08 23:58:50 +0600

    print validate form clean data and error

--- products/views.py
+++ products/views.py
@@ -10,6 +10,11 @@ def product_create_view(request):
     my_form = RawProductForm()
     if request.method == "POST":
         my_form = RawProductForm(request.POST)
+        if my_form.is_valid():
+            #now it is good data
+            print(my_form.cleaned_data)
+        else:
+            print(my_form.errors)
     context = {
         "form": my_form
     }
