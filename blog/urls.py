ac5d909e2c8673dcf1cbb2030a3ad5df862c8fbe
Md Sabbir Rahman <56318666+Sabbir-Rahman@users.noreply.github.com>
2021-01-13 23:57:41 +0600

    class based views

--- blog/urls.py
+++ blog/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 
 from .views import (
     home,
+    ArticleListView,
 )
 
 urlpatterns = [
 
     path('', home, name='blog-home'),
+    path('view/', ArticleListView.as_view(), name='blog-home'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
-
+from .models import Article
 # Create your views here.
+
+from django.views.generic import (
+    CreateView,
+    DetailView,
+    ListView,
+    UpdateView,
+    DeleteView,
+)
+
 
 def home(request):
     return render(request,"blog/home.html")
+
+class ArticleListView(ListView):
+    template_name = 'blog/article_list.html'
+    queryset = Article.objects.all()    # it will generally try to find template in blog/modelname_list.html if we not declare
